25c408418838fe3d35b3fc0e9f6f73186aa29c3f
Masoudi Parrot <masoudi@localhost.localdomain>
2022-03-24 14:58:28 -0400

    Created drag&drop file and functionality

--- src/drag&drop.js
+++ src/drag&drop.js
@@ -4,42 +4,43 @@ export default function dragdrop() {
   const lis = document.querySelectorAll('.ellips');
   const items = document.querySelectorAll('.items > li');
 
-  const dragStart = () => {
-   console.log('Event: ', 'dragStart')
+  let dragStartIndex;
+
+  const dragStart = (e) => {
+   dragStartIndex = +e.target.getAttribute('index');
+   e.target.classList.add('dragStart');
  }
  
  const dragOver = () => {
   console.log('Event: ', 'dragOver')
  }
  
- const dragEnter = () => {
-  console.log('Event: ', 'dragEnter');
-  // this.parentNode.classList.add('over')
+ const dragEnter = (e) => {
+  console.log('Event: ', 'dragEnter', e.target, e.currentTarget);
+  e.currentTarget.parentNode.classList.add('over');
   
  }
  
- const dragDrop = () => {
-  console.log('Event: ', 'dragDrop')
+ const dragDrop = (e) => {
+  console.log('Event: ', 'dragDrop');
+  const dragEndIndex = +e.target.getAttribute('index');
+  console.log(dragEndIndex);
  }
  
- const dragLeave = () => {
+ const dragLeave = (e) => {
   console.log('Event: ', 'dragLeave');
-  // this.parentNode.classList.remove('over')
+  e.currentTarget.parentNode.classList.remove('over');
  }
 
   items.forEach((element) => {
    element.draggable = true;
    element.addEventListener('dragstart', dragStart);
-   element.addEventListener('dragover', dragOver);
-   element.addEventListener('dragenter', dragEnter);
-   element.addEventListener('drop', dragDrop);
-   element.addEventListener('dragleave', dragLeave);
   });
 
   lis.forEach((element) => {
-   element.addEventListener('dragover', dragOver);
-   element.addEventListener('dragenter', dragEnter);
-   element.addEventListener('drop', dragDrop);
-   element.addEventListener('dragleave', dragLeave);
+    element.addEventListener('dragover', dragOver);
+    element.addEventListener('dragenter', dragEnter);
+    element.addEventListener('drop', dragDrop);
+    element.addEventListener('dragleave', dragLeave);
   })
 }
