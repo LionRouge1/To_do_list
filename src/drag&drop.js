982a9317d916b3f572d9edea0b62bc5e3ad5af38
Masoudi Parrot <masoudi@localhost.localdomain>
2022-03-24 20:59:25 -0400

    Created a function to save changes in localStorage

--- src/drag&drop.js
+++ src/drag&drop.js
@@ -1,4 +1,5 @@
-import { forEach } from "lodash";
+import { forEach, get } from "lodash";
+import TDlist from "./TDlist.js";
 
 export default function dragdrop() {
   const lis = document.querySelectorAll('.ellips');
@@ -7,28 +8,81 @@ export default function dragdrop() {
   let dragStartIndex;
 
   const dragStart = (e) => {
-   dragStartIndex = +e.target.getAttribute('index');
+   dragStartIndex = e.target;
    e.target.classList.add('dragStart');
  }
  
- const dragOver = () => {
-  console.log('Event: ', 'dragOver')
+ const dragOver = (e) => {
+  // console.log('Event: ', 'dragOver')
+  e.preventDefault();
  }
  
  const dragEnter = (e) => {
-  console.log('Event: ', 'dragEnter', e.target, e.currentTarget);
   e.currentTarget.parentNode.classList.add('over');
   
  }
  
  const dragDrop = (e) => {
-  console.log('Event: ', 'dragDrop');
-  const dragEndIndex = +e.target.getAttribute('index');
-  console.log(dragEndIndex);
+  e.currentTarget.parentNode.classList.remove('over');
+  items.forEach((li) => li.classList.remove('dragStart'));
+  const dragEndIndex = e.currentTarget.parentNode;
+  const ul = document.getElementById('container_items');
+  const ItemStartIndex = +dragStartIndex.getAttribute('index');
+  const ItemEndIndex = +dragEndIndex.getAttribute('index');
+
+  if (ItemStartIndex < ItemEndIndex) {
+    ul.insertBefore(dragStartIndex, dragEndIndex.nextSibling);
+    const itemIndex = Object.values(items).filter((element) => {
+      const Index = +element.getAttribute('index');
+      
+      if (Index > ItemStartIndex && Index <= ItemEndIndex) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .map((it) => {
+      const n = +it.getAttribute('index') - 1;
+      it.setAttribute('index', n);
+      it.querySelector('.description').id = n;
+    });
+    dragStartIndex.setAttribute('index', ItemEndIndex);
+    dragStartIndex.querySelector('.description').id = ItemEndIndex;
+  }else {
+    ul.insertBefore(dragStartIndex, dragEndIndex);
+    const itemIndex = Object.values(items).filter((element) => {
+      const Index = +element.getAttribute('index');
+      
+      if (Index < ItemStartIndex && Index >= ItemEndIndex) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .map((it) => {
+      const n = +it.getAttribute('index') + 1;
+      it.setAttribute('index', n);
+      it.querySelector('.description').id = n;
+    });
+    dragStartIndex.setAttribute('index', ItemEndIndex);
+    dragStartIndex.querySelector('.description').id = ItemEndIndex;
+  }
+  const tableau = document.querySelectorAll('.items > li');
+  const ntableau = [];
+  tableau.forEach((ite) => {
+    const task = {
+      index: +ite.getAttribute('index'),
+      Tcompleted: (ite.querySelector('.checkbox').getAttribute('completed') === "true") ? true : false,
+      Tdescription: ite.querySelector('.description').value,
+    };
+    ntableau.push(task);
+  });
+  const tdlist = new TDlist();
+  tdlist.saveList(ntableau);
+  console.log(ntableau);
  }
  
  const dragLeave = (e) => {
-  console.log('Event: ', 'dragLeave');
   e.currentTarget.parentNode.classList.remove('over');
  }
 
